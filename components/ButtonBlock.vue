bb36df121eb7a448728fd5ec83718fba4c635e94
MichiWoo <michiwoo.web@gmail.com>
2021-03-03 18:47:36 -0600

    :zap: Agregando inputs, botones, checkbox, genericos

---
+++ components/ButtonBlock.vue
@@ -0,0 +1,8 @@
+<template>
+  <button
+    class="w-full h-12 text-white bg-purple-700 rounded-full hover:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-offset-purple-700 focus:ring-opacity-50"
+    type="submit"
+  >
+    <slot />
+  </button>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  theme: {},
-  variants: {},
-  plugins: [],
   purge: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [require('@tailwindcss/forms')],
 }
