65e1e8783813a2b10a1c39f1306188744dbf0329
MichiWoo <michiwoo.web@gmail.com>
2021-03-10 19:48:56 -0600

    :zap: Agregando Home (Dashboard)

---
+++ pages/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="flex flex-col-reverse items-center justify-center w-full h-screen lg:flex-row"
+  >
+    <div
+      class="flex items-center justify-center w-full bg-purple-200 h-14 lg:h-full lg:w-1/6"
+    >
+      Sidebar
+    </div>
+    <div
+      class="flex items-center justify-center w-full h-full bg-gray-100 lg:h-full lg:w-5/6"
+    >
+      Contenido
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
